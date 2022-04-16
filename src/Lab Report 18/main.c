642bc553476a140463f916118843a16c5e33fa55
kingdragon100 <kabirdeula167@gmail.com>
2022-04-16 14:56:49 +0545

    added Lab 18

---
+++ src/Lab Report 18/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+   int num;
+   FILE *fptr;
+   
+   fptr = fopen("write.txt","w");
+
+   if(fptr == NULL){
+      printf("Error!");   
+      exit(1);             
+   }
+
+   printf("Enter num: ");
+   scanf("%d",&num);
+
+   fprintf(fptr,"%d",num);
+   fclose(fptr);
+
+   return 0;
+}
